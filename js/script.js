6e1fa2c0f3b705947434e8c7a0cc5f280fecd828
Darina <darinaeme@gmail.com>
2022-01-09 20:05:13 +0300

    добавила функционал и умерла

--- js/script.js
+++ js/script.js
@@ -12,6 +12,21 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
+/* Задания на урок:
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+5) Фильмы должны быть отсортированы по алфавиту */
+
+
+
+
 'use strict';
 
 const movieDB = {
@@ -25,29 +40,68 @@ const movieDB = {
 };
 
 let promoAdv = document.querySelectorAll('.promo__adv img');
-promoAdv.forEach(item => item.remove());
+promoAdv.forEach(item => item.remove()); // убрали рекламу
 
 let promoGenre = document.querySelector('.promo__genre');
-promoGenre.textContent = "ДРАМА";
+promoGenre.textContent = "ДРАМА"; // поменяли жанр
 
 let promoBG = document.querySelector('.promo__bg');
-promoBG.style.backgroundImage = 'url("img/bg.jpg")';
+promoBG.style.backgroundImage = 'url("img/bg.jpg")'; // заменили картинку
 
 let interactive = document.querySelector('.promo__interactive-list');
 let oldFilms = interactive.querySelectorAll('li');
-oldFilms.forEach(item => item.remove());
+oldFilms.forEach(item => item.remove()); // удалили html-фильмы
 
 let arrFilms = movieDB['movies'];
-arrFilms.sort();
-console.log(arrFilms);
+arrFilms.sort(); // отсортировали фильмы по алфавиту
 
-for (let i = 0; i < arrFilms.length; i++){
-    interactive.insertAdjacentHTML("beforeend", `<li class="promo__interactive-item">${[i+1] + " " + arrFilms[i]}<div class="delete"></div> </li>`);
-}
+let submitFilm = document.querySelector('.add button');
+submitFilm.addEventListener('click', function(e) {
+    e.preventDefault();
+}); // страница перестала обновляться
+
+    for (let i = 0; i < arrFilms.length; i++){
+    //другой рабочий вариант: interactive.insertAdjacentHTML("beforeend", `<li class="promo__interactive-item">${[i+1] + " " + arrFilms[i]}<div class="delete"></div> </li>`);
+    interactive.innerHTML += `<li class="promo__interactive-item">${[i+1] + " " + arrFilms[i]}<div class="delete"></div> </li>`;
+} // показали фильмы на странице через js массив
 
 
 
+let input = document.querySelector('.adding__input');
 
+
+submitFilm.addEventListener('click', function(){
+    if (input.value != '' && input.value != ' ' && input.value.length <= 21){
+        arrFilms.push(input.value);
+        arrFilms.sort(); 
+        console.log(arrFilms);
+        
+        
+        let newList = document.createElement('ul');
+        newList.classList.add('.promo__interactive-list');
+        for (let i = 0; i < arrFilms.length; i++){
+            newList.insertAdjacentHTML("beforeend", `<li class="promo__interactive-item">${[i+1] + " " + arrFilms[i]}<div class="delete"></div> </li>`);
+            //interactive.innerHTML = `<li class="promo__interactive-item">${[i+1] + " " + arrFilms[i]}<div class="delete"></div> </li>`;
+        } 
+        interactive.replaceWith(newList);
+    } else if (input.value != '' && input.value != ' ' && input.value.length > 21) {
+        let arr = input.value.split('');
+        let newArr = arr.slice(0, 22).join('');
+        newArr += "...";
+
+        arrFilms.push(newArr);
+        arrFilms.sort(); 
+        console.log(arrFilms);
+        
+        let newList = document.createElement('ul');
+        newList.classList.add('.promo__interactive-list');
+        for (let i = 0; i < arrFilms.length; i++){
+            newList.insertAdjacentHTML("beforeend", `<li class="promo__interactive-item">${[i+1] + " " + arrFilms[i]}<div class="delete"></div> </li>`);
+            //interactive.innerHTML = `<li class="promo__interactive-item">${[i+1] + " " + arrFilms[i]}<div class="delete"></div> </li>`;
+        } 
+        interactive.replaceWith(newList);
+    }
+});
 
 
 
